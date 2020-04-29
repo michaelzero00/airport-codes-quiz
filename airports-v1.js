39a480e5a700cc8c25b641e565b6b55ff1d0bfd9
Glitch (hello-webpage) <none>
2020-04-29 09:51:57 +0000

    🍳💅 Checkpoint ./style.css:15646965/1559 ./index.html:15331683/151:15646965/540 ./airports-v1.js:15331683/1278

--- airports-v1.js
+++ airports-v1.js
@@ -7,14 +7,20 @@ var airportCodes = [{iata: "LHR", name: "London Heathrow", city: "London", count
 
 
 // temp script to pull code from array and display it on the front end
-
+  
 //grab the H1 we want to dispaly the IATA code in
 var currentCode = document.getElementById("displayIATACode");
 
-// grab the user input
-var userAnswer = document.getElementById("userGuess");
+// choose a random IATA value from the array. 
+var random = Math.round(Math.random() * airportCodes.length);
 
-// set the H1 element to display an IATA code from the array
-document.getElementById("displayIATACode").textContent = airportCodes[2]["iata"]
+// set the H1 element to display the random IATA code from the array
+document.getElementById("displayIATACode").textContent = airportCodes[random]["iata"]
+
+// grab the user input
+var userAnswer = document.getElementById("userGuess").value;
+
+
+
 
 // compare user answer to 
