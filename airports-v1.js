eb0ddace6451a9ad2b54796d155647270e7c5f2f
Glitch (hello-webpage) <none>
2020-04-29 23:19:57 +0000

    🕶🍁 Checkpoint

--- airports-v1.js
+++ airports-v1.js
@@ -1,9 +1,16 @@
-var airportCodes = [{iata: "LHR", name: "London Heathrow", city: "London", country: "United Kingdom"}, 
+var airportCodes = [
+                    {iata: "LHR", name: "London Heathrow", city: "London", country: "United Kingdom"}, 
                     {iata: "AKL", name: "Auckland International Airport", city: "Auckland", country: "New Zealand"},
                     {iata: "LAX", name: "Los Angeles International Airport", city: "Los Angeles", country: "United States"},
                     {iata: "BOG", name: "El Dorado Luis Carlos Galan Sarmiento International Airport", 
                          city: "Bogota", country: "Colombia"},
-                    {iata: "NRT", name: "Narita International Airport", city: "Tokyo", country: "Japan"}]
+                    {iata: "NRT", name: "Narita International Airport", city: "Tokyo", country: "Japan"},
+                    {iata: "SYD", name: "Sydney International Airport", city: "Sydney", country: "Australia"},
+                    {iata: "MNL", name: "Ninoy Aquino International Airport", city: "Manila", country: "Philippines"},
+                    {iata: "SVQ", name: "Seville Airport", city: "Seville", country: "Spain"},
+                    {iata: "CAN", name: "Guangzhou Baiyun International Airport", city: "Guangzhou", country: "China"},
+                    {iata: "ORD", name: "O'Hare International Airport", city: "Chicago", country: "United States"}
+                   ]
 
 
 // temp script to pull code from array and display it on the front end
