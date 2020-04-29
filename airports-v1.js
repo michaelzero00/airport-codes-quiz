d6ffe8307aeb82cf49bdd60dd072fc6b5616aba7
Glitch (hello-webpage) <none>
2020-04-29 09:15:57 +0000

    🌻💶 Checkpoint ./airports-v1.js:15646965/765 ./index.html:15331683/448

---
+++ airports-v1.js
@@ -0,0 +1,3 @@
+var airportCodes = [{iata: LHR, name: London Heathrow, city: London}, 
+                    {iata: AKL, name: London Heathrow, city: London}
+                    {iata: LHR, name: London Heathrow, city: London}]
