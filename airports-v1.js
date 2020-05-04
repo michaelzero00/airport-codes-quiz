34d506c661f6c3c65258c9ae8a279fe8cf230166
Glitch (hello-webpage) <none>
2020-05-04 04:52:22 +0000

    😬✊ Checkpoint ./index.html:15646965/67 ./airports-v1.js:15646965/2604 ./app-v1.js:15646965/1543

--- airports-v1.js
+++ airports-v1.js
@@ -9,47 +9,12 @@ var airportCodes = [
                     {iata: "MNL", name: "Ninoy Aquino International Airport", city: "Manila", country: "Philippines"},
                     {iata: "SVQ", name: "Seville Airport", city: "Seville", country: "Spain"},
                     {iata: "CAN", name: "Guangzhou Baiyun International Airport", city: "Guangzhou", country: "China"},
-                    {iata: "ORD", name: "O'Hare International Airport", city: "Chicago", country: "United States"}
+                    {iata: "ORD", name: "O'Hare International Airport", city: "Chicago", country: "United States"},
+                    {iata: "YYZ", name: "OToronto Pearson International Airport", city: "Toronto", country: "Canada"},
+                    {iata: "EZE", name: "Ministro Pistarini International Airport", city: "Buenos Aires", country: "Argentina"},
+                    {iata: "SJJ", name: "Sarajevo International Airport", city: "Sarajevo", country: "Bosnia and Herzegovina"},
+                    {iata: "DOH", name: "Hamad International Airport", city: "Doha", country: "Qatar"},
+                    {iata: "KEF", name: "Keflavík International Airport", city: "Reykjavík", country: "Iceland"}
                    ]
 
 
-// temp script to pull code from array and display it on the front end
-  
-// get the H1 we want to dispaly the IATA code in
-var currentCode = document.getElementById("displayIATACode");
-
-// choose a random IATA value from the array. 
-var random = Math.round(Math.random() * airportCodes.length);
-var randomIATACode = airportCodes[random]["iata"];
-
-// set the H1 element to display the random IATA code from the array
-document.getElementById("displayIATACode").textContent = randomIATACode
-
-// get the You Guessed and Correct Answer 
-var youGuessedField = document.getElementById("guessedAnswer");
-var displayCorrectAnswerField = document.getElementById("displayCorrectAnswer");
-
-// grab the user input
-var answerButton = document.getElementById("submit");
-answerButton.addEventListener('click', function(){
-  // test user input against current answer and if correct set alert "you are right"
-
-  // if (document.getElementById("userGuess").value.toLowerCase() === airportCodes[random]["city"].toLowerCase()){
-  //   alert('you are right!')
-  // } else {
-  //   alert ('wrong sucker');
-  // }
-  console.log(document.getElementById("userGuess").value);
-  console.log(airportCodes[random]["city"]);
-  
-  
-  
-  // set the You Guessed field
-  youGuessedField.innerHTML(document.getElementById("userGuess").value) 
-  // set the Correct Answer field
-  displayCorrectAnswerField.innerHTML(airportCodes[random]["city"])
-  
-  
-});
-                              
-                       
