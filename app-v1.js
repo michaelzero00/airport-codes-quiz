caf4c27cac07b89795ab4d749473bee35aaa6ee8
Glitch (hello-webpage) <none>
2020-05-04 09:28:15 +0000

    😚🐡 Checkpoint ./index.html:15331683/3682 ./app-v1.js:15331683/64:15646965/2041 ./airports-v1.js:15331683/35:15646965/70

--- app-v1.js
+++ app-v1.js
@@ -1,3 +1,21 @@
+var airportCodes = [
+                    {iata: "LHR", name: "London Heathrow", city: "London", country: "United Kingdom"}, 
+                    {iata: "AKL", name: "Auckland International Airport", city: "Auckland", country: "New Zealand"},
+                    {iata: "LAX", name: "Los Angeles International Airport", city: "Los Angeles", country: "United States"},
+                    {iata: "BOG", name: "El Dorado Luis Carlos Galan Sarmiento International Airport", city: "Bogota", country: "Colombia"},
+                    {iata: "NRT", name: "Narita International Airport", city: "Tokyo", country: "Japan"},
+                    {iata: "SYD", name: "Sydney International Airport", city: "Sydney", country: "Australia"},
+                    {iata: "MNL", name: "Ninoy Aquino International Airport", city: "Manila", country: "Philippines"},
+                    {iata: "SVQ", name: "Seville Airport", city: "Seville", country: "Spain"},
+                    {iata: "CAN", name: "Guangzhou Baiyun International Airport", city: "Guangzhou", country: "China"},
+                    {iata: "ORD", name: "O'Hare International Airport", city: "Chicago", country: "United States"},
+                    {iata: "YYZ", name: "OToronto Pearson International Airport", city: "Toronto", country: "Canada"},
+                    {iata: "EZE", name: "Ministro Pistarini International Airport", city: "Buenos Aires", country: "Argentina"},
+                    {iata: "SJJ", name: "Sarajevo International Airport", city: "Sarajevo", country: "Bosnia and Herzegovina"},
+                    {iata: "DOH", name: "Hamad International Airport", city: "Doha", country: "Qatar"},
+                    {iata: "KEF", name: "Keflavík International Airport", city: "Reykjavík", country: "Iceland"}
+                   ]
+
 // temp script to pull code from array and display it on the front end
   
 // get the H1 we want to dispaly the IATA code in
@@ -19,11 +37,11 @@ var answerButton = document.getElementById("submit");
 answerButton.addEventListener('click', function(){
   // test user input against current answer and if correct set alert "you are right"
 
-  // if (document.getElementById("userGuess").value.toLowerCase() === airportCodes[random]["city"].toLowerCase()){
-  //   alert('you are right!')
-  // } else {
-  //   alert ('wrong sucker');
-  // }
+  if (document.getElementById("userGuess").value.toLowerCase() === airportCodes[random]["city"].toLowerCase()){
+    alert('you are right!')
+  } else {
+    alert ('wrong sucker');
+  }
   console.log(document.getElementById("userGuess").value);
   console.log(airportCodes[random]["city"]);
   
