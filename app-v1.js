f7215e0c03c113f7fcc5c389f518f1e5ce5e26da
Glitch (hello-webpage) <none>
2020-05-04 10:39:06 +0000

    🌪🏖 Checkpoint ./app-v1.js:15331683/674

--- app-v1.js
+++ app-v1.js
@@ -106,24 +106,29 @@ $(document).ready(function() {
     var random = generateNewQuestion();
     if (
       document.getElementById("userGuess").value.toLowerCase() ===
-      airportCodes[random]["city"].toLowerCase()
+      currentAnswer.toLowerCase()
     ) {
       alert("you are right!");
     } else {
       alert("wrong sucker");
     }
-    console.log(document.getElementById("userGuess").value);
-    console.log(airportCodes[random]["city"]);
+    // console.log(document.getElementById("userGuess").value);
+    // console.log(airportCodes[random]["city"]);
     
     return false;
   });
 });
 
 function generateNewQuestion() {
-  // choose a random IATA value from the array.
+  // choose a random value equal to the length of the IATA array
   var random = Math.round(Math.random() * airportCodes.length);
+  // use this random value to pull an IATA code from the array
   var randomIATACode = airportCodes[random]["iata"];
   // set the H1 element to display the random IATA code from the array
   document.getElementById("displayIATACode").textContent = randomIATACode;
-  return random;
+  // export a string of the location based on this code.
+  var currentAnswer = airportCodes[random]["city"]
+  // export the string value of the airport location
+  console.log("Current value displayed is " + currentAnswer)
+  return currentAnswer;
 }
